ae801cbd610f31290c69e80617c5121c60efe785
viikii007 <vigneshwar797@gmail.com>
2024-05-22 17:31:16 +0530

    jitpack added 4 commit

--- image-lib/build.gradle.kts
+++ image-lib/build.gradle.kts
@@ -34,17 +34,9 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
+     implementation(platform(libs.androidx.compose.bom))
      implementation(libs.androidx.material3)
-     androidTestImplementation(platform(libs.androidx.compose.bom))
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+     debugImplementation(libs.androidx.ui.test.manifest)
 }
 
 afterEvaluate{
